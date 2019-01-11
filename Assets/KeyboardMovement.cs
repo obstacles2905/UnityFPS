e90a2287c505d8ac816e43ece9caacd2b2c0e47d
nikita.pismenniy <nikita.pismenniy@together.com>
2019-01-11 17:12:19 +0200

    initialized a project, house, basic movement and lookup

---
+++ Assets/KeyboardMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Mouse Look/Keyboard Movement")]
+public class KeyboardMovement : MonoBehaviour
+{
+    private CharacterController _character;
+
+    public float movementSpeed = 12f;
+    public float gravity = -9.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * movementSpeed;
+        float deltaZ = Input.GetAxis("Vertical") * movementSpeed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, movementSpeed); // сделано чтоб скорость по диагонали была равна скорости по прямым
+
+        movement.y = gravity; // задание гравитации, чтобы игрок не мог перемещаться по y (летать)
+        movement *= Time.deltaTime; // deltatime задает единую скорость перемещения для всех процессоров
+        movement = transform.TransformDirection(movement); // перевод из локальной системы координат в глобальную
+
+        _character.Move(movement);
+    }
+}
---
+++ Assets/MouseLook.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes {
+        XY = 0,
+        X = 1,
+        Y = 2
+    }
+
+    public RotationAxes axes = RotationAxes.XY;
+
+    public float horizontalSensitivity = 9.0f;
+    public float verticalSensitivity = 9.0f;
+
+    public float minVerticalAngle = -45.0f;
+    public float maxVerticalAngle = 45.0f;
+
+    private float _rotationY = 0; //angle of the vertical turn
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body)
+        {
+            body.freezeRotation = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationX;
+        float delta; 
+
+        switch (axes)
+        {
+            case RotationAxes.XY:
+                _rotationY -= Input.GetAxis("Mouse Y") * verticalSensitivity;
+                _rotationY = Mathf.Clamp(_rotationY, minVerticalAngle, maxVerticalAngle);
+
+                delta = Input.GetAxis("Mouse X") * horizontalSensitivity;
+                rotationX = transform.localEulerAngles.y + delta;
+
+                transform.localEulerAngles = new Vector3(_rotationY, rotationX, 0);
+                break;
+            case RotationAxes.X:
+                delta = Input.GetAxis("Mouse X") * horizontalSensitivity;
+                rotationX = transform.localEulerAngles.y + delta;
+
+                transform.localEulerAngles = new Vector3(0, rotationX, 0);
+                break;
+            case RotationAxes.Y:
+                _rotationY -= Input.GetAxis("Mouse Y") * verticalSensitivity; // increase the rotation angle according to the mouse move
+                _rotationY = Mathf.Clamp(_rotationY, minVerticalAngle, maxVerticalAngle); // fix the rotation angle between min and max values
+
+                rotationX = transform.localEulerAngles.y; //adjust look only for vertical axe
+
+                transform.localEulerAngles = new Vector3(_rotationY, rotationX, 0);
+                break;
+            default:
+                break;
+        }
+    }
+}
